6749ae4409cf2c8145dd75c6216ce14adcd5f67b
Jake Grella <jgrella13@gmail.com>
2021-04-04 12:04:22 -0500

    update register endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -11,62 +11,61 @@ const validateCredentials = require('../middleware/validateCredentials');
 
 // 🌕   REGISTER
 router.post('/register', validateCredentials, async (req, res) => {
-	//password
-	const rounds = process.env.BCRYPT_ROUNDS || 10;
-	const hash = bcrypt.hashSync(req.user.password, rounds);
-	req.user.password = hash;
+  //password
+  const rounds = process.env.BCRYPT_ROUNDS || 10;
+  const hash = bcrypt.hashSync(req.user.password, rounds);
+  req.user.password = hash;
 
-	// change zip code to location_id
-	const location = await Locations.findByZipCode(req.user.zipCode);
-	if (location === undefined) {
-		await Locations.add(req.user.zipCode);
-	}
-	const newLocation = await Locations.findByZipCode(req.user.zipCode);
-	req.user.location_id = newLocation.location_id;
-	delete req.user.zipCode;
-	try {
-		const user = await Users.add(req.user);
-		res.status(201).json(user);
-	} catch (err) {
-		console.log(err.message);
-		res.status(500).json({ errMessage: err.message });
-	}
+  // change zip code to location_id
+  const location = await Locations.findByZipCode(req.user.zipCode);
+  if (location === undefined) {
+    await Locations.add(req.user.zipCode);
+  }
+  const newLocation = await Locations.findByZipCode(req.user.zipCode);
+  req.user.location_id = newLocation.location_id;
+  delete req.user.zipCode;
+  try {
+    const user = await Users.add(req.user);
+    const token = makeToken(user);
+    res.status(201).json({ user: user, token: token });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ errMessage: err.message });
+  }
 });
 
 // 🌕   LOGIN
 router.post('/login', validateCredentials, async (req, res) => {
-	try {
-		const user = await Users.findBy({ username: req.user.username });
-		if (user && bcrypt.compareSync(req.user.password, user.password)) {
-			const token = makeToken(user);
-			process.env.USERNAME = user.username;
-			process.env.USER_ID = user.user_id;
-			process.env.LOCATION_ID = user.location_id;
-			res.status(200).json({
-				message: `welcome ${user.username}`,
-				user: user,
-				token: token,
-			});
-		} else {
-			res.status(401).json({ errMessage: 'invalid credentials' });
-		}
-	} catch (err) {
-		console.log(err.message);
-		res.status(500).json({ errMessage: err.message });
-	}
+  try {
+    const user = await Users.findBy({ username: req.user.username });
+    console.log(req.user.password);
+    console.log(user.password);
+    console.log(bcrypt.compareSync(req.user.password, user.password));
+    if (user && bcrypt.compareSync(req.user.password, user.password)) {
+      const token = makeToken(user);
+      res.status(200).json({
+        message: `welcome ${user.username}`,
+        user: user,
+        token: token
+      });
+    } else {
+      res.status(401).json({ errMessage: 'invalid credentials' });
+      console.log('error in login backend. ');
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ dog: 'dog', errMessage: err.message });
+  }
 });
 
 // 🌕   LOGOUT
 router.post('/logout', validateCredentials, async (_, res) => {
-	try {
-		process.env.USERNAME = '';
-		process.env.USER_ID = 0;
-		process.env.LOCATION_ID = 0;
-		res.status(200).json({ message: 'logout successful' });
-	} catch (err) {
-		console.log(err.message);
-		res.status(500).json({ errMessage: err.message });
-	}
+  try {
+    res.status(200).json({ message: 'logout successful' });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ errMessage: err.message });
+  }
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
 	console.log('listening on port ' + PORT);
